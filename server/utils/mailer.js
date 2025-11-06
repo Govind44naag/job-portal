9b989b95540ad108865d39b64652d3b430debfd3
Govind Naag <govindnaag44@gmail.com>
2025-11-06 14:35:59 +0530

    Add Resend Mail

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,24 +1,26 @@
-import nodemailer from "nodemailer";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendStatusEmail = async (to, jobTitle, status, user) => {
-  const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.EMAIL,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  })
-  await transporter.sendMail({
-    from: `${user.fullName} <${user.email}>`,
-    to: to,
-    replyTo: user.email,
-    subject: `Your Application Status for ${jobTitle}`,
-     html: `
-      <h2>Application Update</h2>
-      <p>Your application for <strong>${jobTitle}</strong> has been
-      <b style="color:${status === "accepted" ? "green" : "red"}">${status}</b>.</p>
-      <br/>
-      <p>Regards,<br/><strong>${user.fullName}</strong></p>
-    `
-  });
+  try {
+    await resend.emails.send({
+      from: `${user.fullName} <onboarding@resend.dev>`, 
+      // NOTE: you can replace domain later with your custom domain
+      to,
+      subject: `Your Application Status for ${jobTitle}`,
+      reply_to: user.email,
+      html: `
+        <h2>Application Update</h2>
+        <p>Your application for <strong>${jobTitle}</strong> has been 
+        <b style="color:${status === "accepted" ? "green" : "red"}">${status}</b>.</p>
+        <br/>
+        <p>Regards,<br/><strong>${user.fullName}</strong></p>
+      `,
+    });
+
+    console.log("Email Sent Successfully");
+  } catch (error) {
+    console.log("Email Sending Failed:", error);
+  }
 };
