d4ce34fa4480f86dc6de657fa603e77473f72bd4
Govind Naag <govindnaag44@gmail.com>
2025-11-06 15:56:50 +0530

    Add Brevo Mail

--- server/utils/mailer.js
+++ server/utils/mailer.js
@@ -1,25 +1,31 @@
-import { Resend } from "resend";
-const resend = new Resend(process.env.RESEND_API_KEY);
+import nodemailer from "nodemailer";
 
 export const sendStatusEmail = async (to, jobTitle, status, user, company) => {
-  await resend.emails.send({
-    from: `${company.name} <onboarding@resend.dev>`,
+  const transporter = nodemailer.createTransport({
+    host: "smtp-relay.brevo.com",
+    port: 587,
+    secure: false,
+    auth: {
+      user: process.env.BREVO_USER,  // usually your gmail
+      pass: process.env.BREVO_SMTP_KEY
+    },
+  });
+
+  await transporter.sendMail({
+    from: `${company.name} <${process.env.BREVO_USER}>`,
     to,
-    reply_to: user.email,
-    subject: `Your Application Status for ${jobTitle} at ${company.name}`,
+    subject: `Application Status for ${jobTitle}`,
     html: `
       <h2>${company.name} - Application Update</h2>
       <p>Your application for <strong>${jobTitle}</strong> has been 
       <b style="color:${status === "accepted" ? "green" : "red"}">${status}</b>.</p>
 
       <h3>Company Details</h3>
-      <p><strong>Company:</strong> ${company.name}</p>
+      <p><strong>Name:</strong> ${company.name}</p>
       <p><strong>Location:</strong> ${company.location}</p>
 
       <br/>
-      <p>Regards,<br/>
-      <strong>${user.fullName}</strong><br/>
-      ${company.name}</p>
+      <p>Regards,<br/><strong>${user.fullName}</strong><br/>${company.name}</p>
     `
   });
 };
